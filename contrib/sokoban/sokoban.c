1a2609571f37043a8333d23f25159eea44af0923
JeranSalcedo <janraymondsalcedo@gmail.com>
2018-05-03 15:45:59 +0800

    Fixed board printing

--- contrib/sokoban/sokoban.c
+++ contrib/sokoban/sokoban.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 #define clear() printf("\n\n\n\n\n\n\n\n\n")
+
 #define TRUE 1
 #define FALSE 2
 
 #define NOT !=
 #define OVER 1
+#define FIRST_LEVEL 1
 
 #define UP 0
 #define LEFT 1
@@ -17,20 +19,23 @@ void map_init(int level);
 void print_board();
 void move(int direction);
 
+//GAME OVER condition
 int GAME = 0;
+
+//MAP VARIABLES
+char **MAP;
 int MAP_LENGTH = 15;
 int MAP_WIDTH = 15;
 
-char **MAP;
-
+//PLAYER VARIABLES
 int player_x, player_y;
 char sokoban_char = 's';
 
 int main(){
 	char command;
 
-	//-----MAP INIT
-	map_init(1);
+	//-----MAP INITIALIZATION
+	map_init(FIRST_LEVEL);
 
 	while(GAME NOT OVER){
 		print_board();
@@ -51,16 +56,16 @@ int main(){
 }
 
 void map_init(int level){
-	int i, x, y;
+	int x, y;
 
 	MAP = (char **) malloc(MAP_WIDTH * sizeof(char *));
-	for(x = 0; x < MAP_WIDTH; x++){
-		MAP[x] = (char *) malloc(MAP_LENGTH * sizeof(char));
+	for(y = 0; y < MAP_LENGTH; y++){
+		MAP[y] = (char *) malloc(MAP_LENGTH * sizeof(char));
 	}
 
 	//-----BOUNDARY
-	for(x = 0; x < MAP_WIDTH; x++){
-		for(y = 0; y < MAP_LENGTH; y++){
+	for(y = 0; y < MAP_LENGTH; y++){
+		for(x = 0; x < MAP_WIDTH; x++){
 			if(x == 0 || x == 14 || y == 0 || y == 14){
 				MAP[x][y] = 'W';
 			} else{
@@ -71,41 +76,181 @@ void map_init(int level){
 
 	switch(level){
 		case 1:{
-			MAP[3][7] = 'b';
-			MAP[6][7] = 'b';
-			MAP[8][7] = 'b';
-			MAP[11][7] = 'b';
+			MAP[7][3] = 'b';
+			MAP[7][6] = 'b';
+			MAP[7][8] = 'b';
+			MAP[7][11] = 'b';
 
-			MAP[5][1] = 'g';
-			MAP[9][1] = 'g';
-			MAP[5][13] = 'g';
-			MAP[9][13] = 'g';
+			MAP[1][5] = 'g';
+			MAP[1][9] = 'g';
+			MAP[13][5] = 'g';
+			MAP[13][9] = 'g';
 
-			MAP[7][7] = sokoban_char;
+			player_x = 7;
+			player_y = 7;
+			MAP[player_x][player_y] = sokoban_char;
 			break;
 		}
 		case 2:{
-			MAP[3][7] = 'b';
+			MAP[6][11] = 'b';
+			MAP[7][3] = 'b';
+			MAP[7][6] = 'b';
+			MAP[7][8] = 'b';
+			MAP[7][11] = 'b';
+			MAP[8][3] = 'b';
 
+			MAP[1][3] = 'g';
+			MAP[1][9] = 'g';
 			MAP[5][1] = 'g';
+			MAP[9][13] = 'g';
+			MAP[13][5] = 'g';
+			MAP[13][11] = 'g';
 
-			MAP[7][7] = sokoban_char;
+			MAP[1][4] = 'W';
+			MAP[2][4] = 'W';
+			MAP[5][7] = 'W';
+			MAP[6][1] = 'W';
+			MAP[6][2] = 'W';
+			MAP[6][3] = 'W';
+			MAP[6][4] = 'W';
+			MAP[6][7] = 'W';
+			MAP[8][7] = 'W';
+			MAP[8][10] = 'W';
+			MAP[8][11] = 'W';
+			MAP[8][12] = 'W';
+			MAP[8][13] = 'W';
+			MAP[9][7] = 'W';
+			MAP[12][10] = 'W';
+			MAP[13][10] = 'W';
+
+			player_x = 7;
+			player_y = 7;
+			MAP[player_x][player_y] = sokoban_char;
 			break;
 		}
 		case 3:{
-			MAP[3][7] = 'b';
+			MAP[4][8] = 'b';
+			MAP[4][9] = 'b';
+			MAP[5][6] = 'b';
+			MAP[5][7] = 'b';
+			MAP[7][6] = 'b';
+			MAP[7][8] = 'b';
+			MAP[8][10] = 'b';
+			MAP[8][11] = 'b';
 
-			MAP[5][1] = 'g';
+			MAP[1][12] = 'g';
+			MAP[3][5] = 'g';
+			MAP[9][13] = 'g';
+			MAP[11][1] = 'g';
+			MAP[11][2] = 'g';
+			MAP[11][4] = 'g';
+			MAP[13][7] = 'g';
 
-			MAP[7][7] = sokoban_char;
+			MAP[1][4] = 'W';
+			MAP[1][9] = 'W';
+			MAP[2][4] = 'W';
+			MAP[2][9] = 'W';
+			MAP[3][4] = 'W';
+			MAP[3][9] = 'W';
+			MAP[3][10] = 'W';
+			MAP[3][11] = 'W';
+			MAP[3][13] = 'W';
+			MAP[4][4] = 'W';
+			MAP[5][4] = 'W';
+			MAP[6][1] = 'W';
+			MAP[6][2] = 'W';
+			MAP[6][3] = 'W';
+			MAP[6][4] = 'W';
+			MAP[6][5] = 'W';
+			MAP[6][6] = 'W';
+			MAP[10][3] = 'W';
+			MAP[10][11] = 'W';
+			MAP[10][12] = 'W';
+			MAP[10][13] = 'W';
+			MAP[11][3] = 'W';
+			MAP[11][8] = 'W';
+			MAP[11][9] = 'W';
+			MAP[11][10] = 'W';
+			MAP[12][1] = 'W';
+			MAP[12][2] = 'W';
+			MAP[12][3] = 'W';
+			MAP[12][4] = 'W';
+			MAP[12][5] = 'W';
+			MAP[12][6] = 'W';
+			MAP[12][8] = 'W';
+			MAP[13][6] = 'W';
+			MAP[13][8] = 'W';
+
+			player_x = 7;
+			player_y = 7;
+			MAP[player_x][player_y] = sokoban_char;
 			break;
 		}
 		case 4:{
-			MAP[3][7] = 'b';
+			MAP[3][8] = 'b';
+			MAP[4][5] = 'b';
+			MAP[4][6] = 'b';
+			MAP[7][6] = 'b';
+			MAP[7][10] = 'b';
+			MAP[8][11] = 'b';
+			MAP[9][9] = 'b';
+			MAP[10][5] = 'b';
 
-			MAP[5][1] = 'g';
+			MAP[1][6] = 'g';
+			MAP[2][6] = 'g';
+			MAP[2][10] = 'g';
+			MAP[3][6] = 'g';
+			MAP[7][1] = 'g';
+			MAP[11][1] = 'g';
+			MAP[11][2] = 'g';
+			MAP[13][7] = 'g';
 
-			MAP[7][7] = sokoban_char;
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+			// MAP[1][4] = 'W';
+
+			player_x = 7;
+			player_y = 7;
+			MAP[player_x][player_y] = sokoban_char;
 			break;
 		}
 		case 5:{
@@ -113,7 +258,9 @@ void map_init(int level){
 
 			MAP[5][1] = 'g';
 
-			MAP[7][7] = sokoban_char;
+			player_x = 7;
+			player_y = 7;
+			MAP[player_x][player_y] = sokoban_char;
 		}
 	}
 }
@@ -122,11 +269,11 @@ void print_board(){
 	int x, y;
 
 	putchar('\n');
-	for(x = 0; x < MAP_WIDTH; x++){
-		for(y = 0; y < MAP_LENGTH; y++){
+	for(y = 0; y < MAP_LENGTH; y++){
+		for(x = 0; x < MAP_WIDTH; x++){
 			printf("%c ", MAP[x][y]);
 		}
-		switch(x){
+		switch(y){
 			case 1: printf("\tCOMMANDS:");
 					break;
 			case 2: printf("\tMOVE UP: W");
