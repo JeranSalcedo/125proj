06da7c767985eef49ec32ddb4c22dab1b99da051
viselvis <abelvis@up.edu.ph>
2018-05-16 17:13:10 +0800

    Demo version

--- contrib/sokoban/sokoban.c
+++ contrib/sokoban/sokoban.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+
+/* Antero, Elvis B 2014-10082 	*/
+/* SAlcedo, Jan Raymond 2014-	*/
+/* CMSC 125 T6L*/
+/* SOKOBAN GAME  */
+
+#include "../../sdk/dexsdk.h"
 
 // DEFINED 
 #define clear() printf("\n\n\n\n\n\n\n\n\n")
-
 #define TRUE 1
 #define FALSE 2
-
 #define NOT !=
 #define OVER 1
 #define UP 0
@@ -15,14 +20,23 @@
 #define DOWN 2
 #define RIGHT 3
 
-// PROTOTYPES 
-//int check_map(int limit);
-void map_init(int level);
+// DEFINE COLORS OF SQUARES
+#define GRASS 19
+#define BOXNOTGS 54
+#define BOXYESGS 36
+#define WALL 56
+#define GOALSQ 64
+#define SOKOBAN 9
+
+/* PROTOTYPES */
+void map_init();
+void print_block(int init_x, int init_y);
 void print_board();
 void changeState0(int direction, int p_y, int p_x);
 void changeState1(int direction, int p_y, int p_x);
 void move(int direction);
 
+/* GLOBAL VARIABLES */
 //GAME OVER condition
 int GAME = 0;
 
@@ -52,17 +66,21 @@ int main(){
 	char command;
 	int i,j, boxesLeft;
 
+	//initialize graphics
+	set_graphics(VGA_320X200X256);
+	
 	//initialize level
-	map_init(level);
+	map_init();
 
 	while(GAME NOT OVER){	
 		printf("\n\n 125 SOKOBAN\n");
+
 		// if there are still boxes
 		if ( boxCounts[level] == 0) {		
 			if (level < 5) {
 				// printf("Level %d completed!\n", level);
 				level = level + 1;
-				map_init(level);
+				map_init();
 			} else {
 				printf(" You finished the game! Congrats! \n");
 				GAME = 1;
@@ -70,10 +88,12 @@ int main(){
 		}
 
 		// print board depending on the level
-		print_board(level);
+		print_board();
 
 		printf(" Input move: ");
-		scanf(" %c", &command);
+		command = (char)getch();
+
+		//scanf(" %c", &command);
 
 		// w/W is for up
 		if(command == 'w' || command == 'W'){
@@ -94,10 +114,14 @@ int main(){
 		} else {
 			printf(" That's an invalid input!\n");
 		}
+
 	}
+
+	set_graphics(VGA_TEXT80X25X16);
+	clrscr();
 }
 
-void map_init(int level){
+void map_init(){
 	int x, y;
 
 	// allocate for map
@@ -110,9 +134,9 @@ void map_init(int level){
 	for(y = 0; y < MAP_LENGTH; y++){
 		for(x = 0; x < MAP_WIDTH; x++){
 			if(x == 0 || x == 14 || y == 0 || y == 14){
-				MAP[y][x] = 'W';
+				MAP[x][y] = 'W';
 			} else{
-				MAP[y][x] = ' ';
+				MAP[x][y] = ' ';
 			}
 		}
 	}
@@ -379,19 +403,93 @@ void map_init(int level){
 	}
 }
 
-// prints the present state of the map
-void print_board(int levelStage){
+void print_block(int init_x, int init_y){
 	int x, y;
 
-	putchar('\n');
-	printf(" LEVEL %d - - - - - - - - - - - \n", levelStage);
-	printf(" Boxes left: %d\n\n", boxCounts[level]);
+	//int color;
+	//int type = MAP[][]
 
+	const int X_PADDING = 10;
+	const int Y_PADDING = 40;
+
+	init_x *= 10;
+	init_y *= 10;
+	
+	write_pixel(init_x, init_y, 100);
+	// for (y = 0; y < 10; y++){
+	// 	for (x = 0; x < 10 ; x++){
+	// 	}
+	// }
+	
+	//write_pixel();
+}
+
+// prints the present state of the map
+void print_board(){
+	// 
+	int X_PADDING = 10;
+	int Y_PADDING = 40;
+	int x, y, i, j ;
+	
+	char message_level[2];
+	char message_boxes[3];
+
+	// for block
+	int color = 0;
+	int type;
+
+
+	//printf(" \nLEVEL %d - - - - - - - - - - - \n", levelStage);
+	//printf(" Boxes left: %d\n\n", boxCounts[level]);
+	sprintf(message_level, "%d", level);
+	sprintf(message_boxes, "%d", level);
+
+	clrscr();
+
+	// (level , )
+	write_text("LEVEL", 10, 10, 63, 0);
+	write_text(message_level, 70, 10, 63, 0);
+	write_text("Boxes left: ", 10, 20, 63, 0);
+	write_text(message_boxes, 150, 20, 63, 0);
+
+	write_text("COMMANDS: ", 170, 30, 63, 0);
+	write_text("Move up: w", 170, 40, 63, 0);
+	write_text("Move left: a", 170, 50, 63, 0);
+	write_text("Move down: s", 170, 60, 63, 0);
+	write_text("Move right: d", 170, 70, 63, 0);
+	write_text("Exit: x", 170, 80, 63, 0);
+
+	for (y = 0; y < MAP_LENGTH; y++){
+		for(x = 0; x < MAP_WIDTH; x++){
+			
+			type = MAP[x][y];
+
+			switch(type){
+				case 'b': color = BOXNOTGS; break;
+				case 'B': color = BOXYESGS; break;
+				case ' ': color = GRASS; break;
+				case 'W': color = WALL; break;
+				case 's': color = SOKOBAN; break;
+				case 'S': color = SOKOBAN; break;
+				case 'g': color = GOALSQ;
+			}
+
+			for (i=Y_PADDING; i<Y_PADDING + 10; i++){
+				for (j=X_PADDING; j<X_PADDING + 10; j++){
+					write_pixel(x * 10 + j, y * 10 + i, color);
+				}
+			}			
+			//write_pixel(x*10, y*10, color);
+			//color = color + 1;
+		}
+	}
+	/*
 	// prints the map and the commands
 	for(y = 0; y < MAP_LENGTH; y++){
 		for(x = 0; x < MAP_WIDTH; x++){
 			printf("%c ", MAP[x][y]);
 		}
+		
 		switch(y){
 			case 1: printf("\tCOMMANDS:");
 					break;
@@ -417,8 +515,10 @@ void print_board(int levelStage){
 					break;
 			case 13: printf("\tg - goal square");
 		}
-		putchar('\n');
+		printf("\n");
+		
 	}
+	*/
 }
 
 // changes state of the sokoban (s)
